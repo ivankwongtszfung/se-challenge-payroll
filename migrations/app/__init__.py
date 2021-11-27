76ac2a0f8738a79aa504c9a16fc90c74a27a0f58
Ivan Kwong <ivankwongtszfung@gmail.com>
2021-11-27 21:55:15 +0800

    feat(migration): link up app models

---
+++ migrations/app/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+__path__ = [os.path.join(os.path.dirname(__file__), "..", "..", "app")]
--- migrations/env.py
+++ migrations/env.py
@@ -15,9 +15,9 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models import base
+target_metadata = base.Base.metadata
+config.set_main_option("sqlalchemy.url", base.SQLALCHEMY_DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
